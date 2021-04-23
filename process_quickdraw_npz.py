c4198d627691445e6b2c6039c7209990559634b6
ivan <ivan.puhachov@gmail.com>
2021-04-22 21:35:37 -0400

    clear npz data pipeline

--- process_quickdraw_npz.py
+++ process_quickdraw_npz.py
@@ -4,27 +4,33 @@ from joblib import Parallel, delayed
 import matplotlib.pyplot as plt
 
 
-dataname = "owl"
-n_items = 10000
-finalsize = 32
-subset = 'train'
-
-input_path = f"data/{dataname}.npz"
-out_path = f"data/bitmap_{dataname}_{subset}_{finalsize}x{n_items}.npy"
-
-
 def process_npz_item(item, img_size=32):
     drawing = Drawing.from_npz_data(item)
     npimg = drawing.draw_to_numpy(img_size=img_size, linewidth=6)
     return npimg
 
+def process_npz(dataname='owl', n_items=10000, finalsize=32):
+    subset = 'train'
 
-data = np.load(input_path, encoding='latin1', allow_pickle=True)
-data_resized = data[subset][:n_items]
-bitmaps = Parallel(n_jobs=4, verbose=10)(delayed(process_npz_item)(data_resized[i], finalsize) for i in range(n_items))
-bitmaps = np.array(bitmaps)
-plt.imshow(bitmaps[1], cmap='gray_r')
-plt.colorbar()
-plt.show()
-print(bitmaps.shape)
-np.save(out_path, bitmaps)
+    input_path = f"data/{dataname}.npz"
+    out_path = f"data/bitmap_{dataname}_{subset}_{finalsize}x{n_items}.npy"
+
+
+    data = np.load(input_path, encoding='latin1', allow_pickle=True)
+    data_resized = data[subset][:n_items]
+    bitmaps = Parallel(n_jobs=8, verbose=10)(delayed(process_npz_item)(data_resized[i], finalsize) for i in range(n_items))
+    bitmaps = np.array(bitmaps)
+    plt.imshow(bitmaps[1], cmap='gray_r')
+    plt.colorbar()
+    plt.show()
+    print(bitmaps.shape)
+    np.save(out_path, bitmaps)
+
+
+if __name__=="__main__":
+    process_npz("owl")
+    process_npz("apple")
+    process_npz("cat")
+    process_npz("carrot")
+    process_npz("creativebirds")
+    process_npz("fish")
